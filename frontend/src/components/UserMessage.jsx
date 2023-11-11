de623c5380730195dc6d786c4d36cef8ed885f30
Noel George C J <noelgeorge2004@gmail.com>
2023-11-11 18:38:12 +0530

    Save progress - Textinput added

--- frontend/src/components/UserMessage.jsx
+++ frontend/src/components/UserMessage.jsx
@@ -4,7 +4,7 @@ const UserMessage = (props) => {
     const { message } = props;
   return (
     <div className='flex justify-end w-full'>
-        <div className='bg-[#5E9FF2] w-3/5 p-3 mr-9 mt-6 rounded-md'>
+        <div className='bg-[#94BDF1] w-3/5 p-3 mr-9 mt-6 rounded-md'>
             { message }
         </div>
     </div>
